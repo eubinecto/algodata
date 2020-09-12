28a99900fcc75301a5fb41b5e05323722060b2cf
Eu-Bin KIM <tlrndk123@gmail.com>
2020-09-12 23:48:56 +0900

    implementing stack in java.

---
+++ src/stacksAndQueues/java/MyStack.java
@@ -0,0 +1,66 @@
+package stacksAndQueues.java;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+// using generic
+/*
+ * Stack is a LIFO data structure.
+ * Last In First Out.
+ */
+public class MyStack<T> {
+    // inner data class for stack
+    // houses data with the same generic data type T
+    private static class StackNode<T> {
+        private final T data; // data is final
+        private StackNode<T> next; // but next node should not be final
+
+        public StackNode(T data) {
+            this.data = data;
+            // if not assigned, the value will be null.
+            // here, next node is not assigned,so the value of it
+            // is null
+        }
+    } // Stack Node
+    //the stack node maintains this.
+    private StackNode<T> top;
+
+    /*
+    TO-DO
+    implement the following ADT interface for the stack
+    pop()
+    push(item)
+    peek()
+    isEmpty()
+     */
+    public T pop() {
+        if (this.isEmpty()) {
+            // Ha! so there is an exception class like this.
+            throw new EmptyStackException();
+        }
+        T poppedData = this.top.data;
+        this.top = this.top.next;
+        return poppedData;
+    }
+
+    public void push(T item) {
+        StackNode<T> newNode = new StackNode<>(item);
+        newNode.next = this.top; // the top is the next one to the new node
+        this.top = newNode; // newNode is now at the top
+    }
+
+    public T peek() {
+        if (this.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        // just return the data
+        return this.top.data;
+    }
+
+    public boolean isEmpty() {
+        // how do I check if the stack is empty?
+        // just check this out
+        return this.top == null;
+    }
+
+}
---
+++ src/stacksAndQueues/python/data.py
@@ -0,0 +1,17 @@
+
+
+# mutable object is allowed for dataclasses
+class MyStack:
+    """
+    A stack is a FIFO data structure
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class MyQueue:
+    """
+    A queue is a LIFO data structure
+    """
+    pass
